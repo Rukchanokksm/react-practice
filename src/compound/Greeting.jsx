05f5d9f4b3df55e153abda2d6ff1e59dc5834128
rukchanok krumsamer <pepoil07@hotmail.com>
2023-06-07 17:09:17 +0700

    Update Learn React

--- src/compound/Greeting.jsx
+++ src/compound/Greeting.jsx
@@ -1,14 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 import classes from "./Greeting.module.css"
 const Greeting = (props) => {
-  const { name } = props
-  const isLogin = false
+  // const state = useState("Smite")
+  const [name, setName] = useState("Smite")
+  const handleClick = () => {
+    setName("John")
+  }
   return (
-    <div className={classes.card}>
-      <h3>Wellcome</h3>
-      <p>{isLogin ? name : "Unknow"}</p>
-    </div>
+    <>
+      <div className={classes.card}>
+        <h3>Wellcome</h3>
+        <p>{name}</p>
+      </div>
+      <button onClick={handleClick}>Change Name</button>
+    </>
   )
+
+  // const { name } = props
+  // const isLogin = false
+  // return (
+  //   <div className={classes.card}>
+  //     <h3>Wellcome</h3>
+  //     <p>{isLogin ? name : "Unknow"}</p>
+  //   </div>
+  // )
 }
 
 export default Greeting
--- src/compound/Post.jsx
+++ src/compound/Post.jsx
@@ -1,17 +1,50 @@
-import React from "react"
+import React, { useState } from "react"
 import classes from "./Post.module.css"
+
+const content = [
+  {
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    text: "Lorem ipsum dolor sit amet.",
+  },
+  {
+    text: "Lorem, ipsum dolor.",
+  },
+]
 
 const Post = (props) => {
   const { post } = props
-
   const handleClick = () => {
     alert(`Post ${post.id} clicked!`)
   }
 
+  const [isShow, setIsShow] = useState(false)
+
+  const toggleShow = () => {
+    setIsShow(!isShow)
+  }
+
   return (
+    // <div onClick={() => handleClick(post.id)} className={classes.poststyle}>
+    //   <p>ID : {post.id}</p>
+    //   <p>TITLE : {post.title}</p>
+    //   <button>showmore</button>
+    // </div>
     <div onClick={() => handleClick(post.id)} className={classes.poststyle}>
       <p>ID : {post.id}</p>
       <p>TITLE : {post.title}</p>
+      {/* {
+        content.map((con) =>{
+      return {isShow && {con.text}}   
+        })} */}
+      {isShow && (
+        <p>
+          Lorem ipsum dolor sit amet, vitae unde magnam architecto quasi ullam? Iure perspiciatis labore doloremque
+          odio....
+        </p>
+      )}
+      <button onClick={toggleShow}>{isShow ? "Show Less" : "Show More"}</button>
     </div>
   )
 }
